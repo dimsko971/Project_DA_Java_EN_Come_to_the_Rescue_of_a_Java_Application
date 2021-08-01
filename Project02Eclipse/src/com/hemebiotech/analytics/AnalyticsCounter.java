2d6fa2afcb358623ada78f53e1fe3d810de15521
dimitri <dimsko9711@gmail.com>
2021-08-01 17:44:54 +0200

    Utilisation de ISymptomReader qui permet les gestion des exceptions du fichier donné en entrée

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,38 +3,39 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.*;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	private static int headacheCount = 0; // initialize to 0
+	private static int rashCount = 0; // initialize to 0
+	private static int pupilCount = 0; // initialize to 0
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
+	public static void main(String args[]) throws Exception {
+		
+		ISymptomReader symptomReader = new ReadSymptomDataFromFile("C:\\Users\\33629\\git\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt");
+		/*List list = symptomReader.GetSymptoms();
+				
+		int i = 0; // set i to 0
+		int headCount = 0; // counts headaches
+		while (i < list.size()) {
+			String line = (String) list.get(i);
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
 				headCount++;
 				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
+			} else if (line.equals("rush")) {
 				rashCount++;
-			}
-			else if (line.contains("pupils")) {
+			} else if (line.contains("pupils")) {
 				pupilCount++;
 			}
 
-			line = reader.readLine();	// get another symptom
-		}
-		
+			i++; // increment i
+		}*/
+
 		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
+		FileWriter writer = new FileWriter("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
